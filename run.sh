bc550b3f1a8d93e6c9f24d32c4501830cd1a90a8
termie <github@anarkystic.com>
2014-11-18 21:38:55 +0100

    initial:

---
+++ run.sh
@@ -0,0 +1,22 @@
+
+if [ ! -n "$WERCKER_BASH_TEMPLATE_INPUT" ]; then
+  export WERCKER_BASH_TEMPLATE_INPUT="*.template"
+fi
+
+if [ ! -n "$WERCKER_BASH_TEMPLATE_OUTPUT" ]; then
+  export WERCKER_BASH_TEMPLATE_OUTPUT="./"
+fi
+
+
+for input in $WERCKER_BASH_TEMPLATE_INPUT; do
+  outname=${input/\.template/}
+  case "$WERCKER_BASH_TEMPLATE_OUTPUT" in
+    */)
+      output="$WERCKER_BASH_TEMPLATE_OUTPUT$outname"
+      ;;
+    *)
+      output=$WERCKER_BASH_TEMPLATE_OUTPUT
+  esac
+  echo "Templating $input -> $output"
+  ./template.sh < "$input" > "$output"
+done
