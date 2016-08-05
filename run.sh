4f99bf81cdcb6df1901fc49e68159216d6d57bd2
termie <github@anarkystic.com>
2016-08-05 14:21:02 +0200

    get working with posix

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#!/bin/sh
+DIR=$WERCKER_STEP_ROOT
 if [ ! -n "$WERCKER_BASH_TEMPLATE_INPUT" ]; then
   export WERCKER_BASH_TEMPLATE_INPUT="*.template*"
 fi
@@ -10,7 +10,7 @@ fi
 
 
 for input in $WERCKER_BASH_TEMPLATE_INPUT; do
-  outname=${input//\.template/}
+  outname=$(echo "$input" | sed 's/\.template//')
   case "$WERCKER_BASH_TEMPLATE_OUTPUT" in
     */)
       output="$WERCKER_BASH_TEMPLATE_OUTPUT$outname"
