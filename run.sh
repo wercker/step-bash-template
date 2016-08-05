6c208790934f060fb92ab5287af32b3d10abebb8
termie <github@anarkystic.com>
2016-08-05 15:28:22 +0200

    remove default

--- run.sh
+++ run.sh
@@ -8,7 +8,6 @@ if [ ! -n "$WERCKER_BASH_TEMPLATE_OUTPUT" ]; then
   export WERCKER_BASH_TEMPLATE_OUTPUT="./"
 fi
 
-
 for input in $WERCKER_BASH_TEMPLATE_INPUT; do
   outname=$(echo "$input" | sed 's/\.template//')
   case "$WERCKER_BASH_TEMPLATE_OUTPUT" in
