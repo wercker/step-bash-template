cfd8b6399c1fea3d71cbe6406e351b2538916fb2
Andy Smith <termie@users.noreply.github.com>
2017-04-05 16:30:18 -0700

    cross-shell error-on-empty vars for templates (#5)
    * get working with posix
    
    * remove default
    
    * hmmmm
    
    * add error on empty
    
    * add error-on-empty
    
    * add the ewok.yml for new-style testing

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#!/bin/sh
+DIR=$WERCKER_STEP_ROOT
 if [ ! -n "$WERCKER_BASH_TEMPLATE_INPUT" ]; then
   export WERCKER_BASH_TEMPLATE_INPUT="*.template*"
 fi
@@ -8,9 +8,8 @@ if [ ! -n "$WERCKER_BASH_TEMPLATE_OUTPUT" ]; then
   export WERCKER_BASH_TEMPLATE_OUTPUT="./"
 fi
 
-
 for input in $WERCKER_BASH_TEMPLATE_INPUT; do
-  outname=${input//\.template/}
+  outname=$(echo "$input" | sed 's/\.template//')
   case "$WERCKER_BASH_TEMPLATE_OUTPUT" in
     */)
       output="$WERCKER_BASH_TEMPLATE_OUTPUT$outname"
--- template.sh
+++ template.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
+if [ ! -z "$WERCKER_BASH_TEMPLATE_ERROR_ON_EMPTY" ]; then
+  set -eu
+fi
 
-eval "cat <<EOF
+if [ -e /dev/urandom ]; then
+  RANDO=$(LANG=C tr -cd 0-9 </dev/urandom | head -c 12)
+else
+  RANDO=2344263247
+fi
+
+eval "cat 2> $RANDO <<EOF
 $(cat "$1")
-EOF"
+"
+
+if [ ! -z "$WERCKER_BASH_TEMPLATE_ERROR_ON_EMPTY" ]; then
+  if [ -s $RANDO ]; then
+    rm -f $RANDO
+    exit 1
+  fi
+fi
+rm -f $RANDO
