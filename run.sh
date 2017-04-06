8d795d9b2ebb0dcbd1f92d10c5f54cbd9619c6a3
Andy Smith <termie@users.noreply.github.com>
2017-04-06 15:25:18 -0700

    move error check into run.sh (#6)
    * move the check code into run.sh
    
    * clean up better
    
    * exit when we want
    
    * comment
    
    * test with the step itself via dev steps

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+set +e
+# This set +e is here because -u operates differently between ash and bash
+
 DIR=$WERCKER_STEP_ROOT
 if [ ! -n "$WERCKER_BASH_TEMPLATE_INPUT" ]; then
   export WERCKER_BASH_TEMPLATE_INPUT="*.template*"
@@ -18,5 +21,21 @@ for input in $WERCKER_BASH_TEMPLATE_INPUT; do
       output=$WERCKER_BASH_TEMPLATE_OUTPUT
   esac
   echo "Templating $input -> $output"
-  "$DIR/template.sh" "$input" > "$output"
+
+  if [ -e /dev/urandom ]; then
+    RANDO=$(LANG=C tr -cd 0-9 </dev/urandom | head -c 12)
+  else
+    RANDO=2344263247
+  fi
+
+  "$DIR/template.sh" "$input" > "$output" 2>$RANDO
+
+  if [ ! -z "$WERCKER_BASH_TEMPLATE_ERROR_ON_EMPTY" ]; then
+    if [ -s $RANDO ]; then
+      cat $RANDO
+      rm -f $RANDO
+      exit 1
+    fi
+  fi
+  rm -f $RANDO
 done
--- template.sh
+++ template.sh
@@ -1,22 +1,8 @@
 #!/bin/sh
 if [ ! -z "$WERCKER_BASH_TEMPLATE_ERROR_ON_EMPTY" ]; then
-  set -eu
+  set -u
 fi
 
-if [ -e /dev/urandom ]; then
-  RANDO=$(LANG=C tr -cd 0-9 </dev/urandom | head -c 12)
-else
-  RANDO=2344263247
-fi
-
-eval "cat 2> $RANDO <<EOF
+eval "cat <<EOF
 $(cat "$1")
 "
-
-if [ ! -z "$WERCKER_BASH_TEMPLATE_ERROR_ON_EMPTY" ]; then
-  if [ -s $RANDO ]; then
-    rm -f $RANDO
-    exit 1
-  fi
-fi
-rm -f $RANDO
